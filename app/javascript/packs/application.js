79dbece80dcc9a68f65adf47f15e2c2dfd6d465b
Paul Anthony McGowan <xhostcom@gmail.com>
2021-10-27 18:56:32 +0100

    First Commit

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,30 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+import 'bootstrap/dist/js/bootstrap'
+
+import TurbolinksAdapter from 'vue-turbolinks'
+import Vue from 'vue/dist/vue.esm'
+import App from '../app.vue'
+
+Vue.use(TurbolinksAdapter)
+
+Vue.component('app', App)
+
+ document.addEventListener('turbolinks:load', () => {
+   const app = new Vue({
+     el: '[data-behaviour="vue"]',
+    })
+    console.log(app)
+})
+
